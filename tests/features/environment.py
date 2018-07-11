957b66e48096fe2e70b0f5eb2dbdf64567b5e7db
ladaflac <lada.flac@gmail.com>
2018-07-11 09:48:36 +0200

    screenshot filename changed to contain the scenario name

--- tests/features/environment.py
+++ tests/features/environment.py
@@ -4,5 +4,5 @@ import os
 
 def after_step(context, step):
     if step.status == 'failed':
-        filename = "login_" + datetime.now().strftime('%Y%m%d%H%M%S') + ".png"
+        filename = context.scenario.name + "_" + datetime.now().strftime('%Y%m%d%H%M%S') + ".png"
         context.driver.save_screenshot((os.path.join(os.path.expanduser('~\\Downloads'), filename)))
