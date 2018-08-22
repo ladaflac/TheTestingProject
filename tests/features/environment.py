b9438d590705bbb9f9d43c785366b9ca4f0a2f40
ladaflac <lada.flac@gmail.com>
2018-08-22 21:36:27 +0200

    chrome options added to support headless mode

--- tests/features/environment.py
+++ tests/features/environment.py
@@ -1,10 +1,14 @@
+from selenium.webdriver.chrome.options import Options
 from selenium import webdriver
 from datetime import datetime
 import os
 
 
 def before_all(context):
-    context.driver = webdriver.Chrome('C:\Windows\Webdriver\chromedriver.exe')
+    options = Options()
+    options.headless = False
+    options.add_argument('disable-gpu')
+    context.driver = webdriver.Chrome('C:\Windows\Webdriver\chromedriver.exe', chrome_options=options)
     context.driver.get('https://ebanking-demo-ch1.ubs.com/auth/login1?userId=DEMO9999&languageCode=en')
 
 def after_step(context, step):
