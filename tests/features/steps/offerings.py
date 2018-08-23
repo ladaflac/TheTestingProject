3980f583732ea763fceddb931f1296871bfc0676
ladaflac <lada.flac@gmail.com>
2018-08-23 17:50:19 +0200

    delete unused import

--- tests/features/steps/offerings.py
+++ tests/features/steps/offerings.py
@@ -2,7 +2,7 @@ from behave import given, then
 from selenium.webdriver.support import expected_conditions
 from selenium.webdriver.support.wait import WebDriverWait
 from page_model.base_page import BasePage
-from page_model.locators import OfferingsLocators, CommonLocators
+from page_model.locators import OfferingsLocators
 
 
 @given("The Offerings page is opened")
